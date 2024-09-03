3d0e3010ed5c1bcf2c64a452c74f45f16ffe06e4
yahya-salhi <pcclub10@gmail.com>
2024-09-03 22:59:03 +0100

    add inital commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/Homepage";
+import "./App.css";
+
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/AppLayout.tsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <PageNav />
+      <p>APP</p>
+    </div>
+  );
+}
