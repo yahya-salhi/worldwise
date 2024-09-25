af323393c6c84e8959f26e200d92c3be7a2eae73
yahya-salhi <pcclub10@gmail.com>
2024-09-25 16:38:40 +0100

    handel the user Auth and AuthContext api

--- src/App.tsx
+++ src/App.tsx
@@ -13,31 +13,34 @@ import CountriesList from "./components/CountriesList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 export default function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              // element={<CityList cities={cities} isLoading={isLoding} />}
-              // use automatic Navigation
-              element={<Navigate replace to="cities" />}
-            />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountriesList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route
+                index
+                // element={<CityList cities={cities} isLoading={isLoding} />}
+                // use automatic Navigation
+                element={<Navigate replace to="cities" />}
+              />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountriesList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,11 +1,13 @@
 import Sidebar from "../components/Sidebar";
 import styles from "../pages/AppLayout.module.css";
 import Map from "../components/Map";
+import User from "../components/User";
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
